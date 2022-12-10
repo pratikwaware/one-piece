c56d1714160a560ed5d1984e29ef361093b4e3b2
pratikwaware <pratikwavare919@gmail.com>
2022-12-10 20:35:16 +0530

    build setup 3

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {appDir: true},
-  exportTrailingSlash: true,
+  exportTrailingSlash: false,
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
